8c67d52eb5017a5ac276c7f2070a9c443373ba4e
timpetliar <tdp259@nyu.edu>
2017-07-15 21:39:50 -0400

    Add files via upload

---
+++ simulation.cpp
@@ -0,0 +1,296 @@
+using namespace std;
+#include <iostream> 
+#include <fstream>
+
+#define GIDX(i,j) (i*num_boxes+j)
+#define NEW_RELEASE_WEEK 156
+#define INFTY 10000
+#define WEEKS_TO_BUNDLING 0
+#define BUNDLING_WEEK NEW_RELEASE_WEEK + WEEKS_TO_BUNDLING 
+class G_Array {
+//private data
+	private:
+
+	public:
+		double eta, phi, nu, psi, tau, omega, step_size;
+		double f;
+		double *g, *revenue, *rev_bundle, *rev_sep;
+		int num_boxes, prices_size, revenue_size;
+		bool bundle;
+		static const int prn_log = 1;
+
+//public methods
+	public:
+		G_Array(double eta, double phi, double  nu, double psi, double tau, int num_boxes, int prices_size);
+		virtual ~G_Array();
+		double update_cell_purchases(int i, int j);
+		double update_cell_no_purchases(int i, int j);
+		double update_zero_box(int i, int j);
+		virtual void initialize_array() = 0;
+		double sales_revenues();
+		double buy_bundle(double p_b, int i, int j);
+		double buy_1(double p_t_1, int i, int j);	
+		double buy_2(double p_t_2, int i, int j);
+		double buy_both(double p_t_1, double p_t_2, int i, int j);
+		void buy_none(int i, int j);
+		
+		
+}; //close class definition
+	
+G_Array::G_Array( double eta, double phi, double  nu, double psi, double tau, int num_boxes, int prices_size): eta{eta}, phi{phi}, nu{nu}, psi{psi}, tau{tau}, num_boxes{num_boxes}, prices_size{prices_size}, bundle{false} {
+		step_size = psi / (num_boxes - 1);
+		f = 0; 					//should be re-initialized in dervived classes
+		g = new double [num_boxes * num_boxes];
+		revenue = new double[prices_size];                                 
+		rev_bundle = new double[prices_size];
+		rev_sep = new double[prices_size];
+}
+G_Array::~G_Array(){
+	delete[] g; 
+	delete[]revenue ;
+	delete[] rev_bundle;
+	delete[]  rev_sep;
+}
+double inline G_Array::update_cell_purchases(int i, int j) {
+	return ( (1 - phi) *(1 - nu) * g[GIDX(i,j)] + nu * eta * f * step_size * step_size );
+}
+double inline G_Array:: update_cell_no_purchases(int i, int j) {
+	return ( (1- nu) * g[GIDX(i,j)] + nu * eta * f * step_size * step_size ); 
+}
+double inline G_Array:: update_zero_box(int i, int j){
+	return ( phi * (1 - nu) * g[GIDX(i,j)] );
+}
+double inline G_Array::buy_bundle(double p_b, int i, int j){
+	double g_new = update_cell_purchases(i, j);
+	g[GIDX(0,0)] = update_zero_box(i, j);
+	g[GIDX(i,j)] = g_new;
+	return (p_b * phi * g[GIDX(i,j)]);
+}
+double inline G_Array::buy_1(double p_t_1, int i, int j){
+	double g_new = update_cell_purchases(i, j);
+        g[GIDX(0,j)] = update_zero_box(i, j);
+        g[GIDX(i,j)] = g_new;
+        return (p_t_1 * phi * g[GIDX(i,j)]);
+}
+double inline G_Array::buy_2(double p_t_2, int i, int j){
+	double g_new = update_cell_purchases(i, j);
+	g[GIDX(i,0)] = update_zero_box(i, j);
+	g[GIDX(i,j)] = g_new;
+	return (p_t_2 * phi * g[GIDX(i,j)]);
+}
+double inline G_Array::buy_both(double p_t_1, double p_t_2, int i, int j){
+	double g_new = update_cell_purchases(i, j);
+	g[GIDX(0,0)] = update_zero_box(i, j);
+	g[GIDX(i,j)] = g_new;
+	return ( (p_t_1 + p_t_2) * phi * g[GIDX(i,j)] );
+}
+void inline G_Array:: buy_none(int i, int j){
+	g[GIDX(i,j)] = update_cell_no_purchases(i, j);
+	return;
+}
+
+
+/* -------------------------------------------------------------------------------------------------------------- */
+
+
+class Bundle_Simulation {
+	public:
+		double *prices;
+		void simulate_path(int time);
+		double compute_val_both(int i, int j);
+		double compute_val_1(int i, int j);
+		double compute_val_2(int i, int j);
+		bool check_for_bundle(double p_b, int i, int j);
+		bool bundle_check_p_1(double p_t_1, int i, int j);
+		bool bundle_check_p_2(double p_t_2, int i, int j);
+		bool check_1(double p_t_1, int i, int j);
+		bool check_2(double p_t_2, int i, int j);
+		void g_update(double p_t_1, double p_t_2, double  a_t_1, double a_t_2, int t);
+		double discount(int t);							// TBD
+		double *revenue, *rev_bundle, *rev_sep;
+		double omega, tau; 
+		Bundle_Simulation(double omega, double tau, int prices_size);
+		
+			
+
+	private:
+		G_Array *g;
+		double a_t_1, a_t_2;					//discount factor
+		int prices_size;
+};
+Bundle_Simulation::Bundle_Simulation(double omega, double tau, int prices_size): omega{omega}, tau{tau}, prices_size{prices_size}{}
+double Bundle_Simulation::discount (int t) {
+    double a_t;
+    if (t <= tau)
+        a_t = 1;
+    else
+       a_t = omega + (1-omega) * ( (double)tau/(double)t );
+    return a_t;
+}
+
+
+void Bundle_Simulation::simulate_path(int time){
+    double p_t_1, p_t_2, a_t_1, a_t_2;                  //weekly price and dicount factor for DVD1 and DVD2
+	int t;
+    for (t=0; t<NEW_RELEASE_WEEK; t++) {                        //Only first DVD has been released
+        p_t_1 = prices[t];
+        p_t_2 = INFTY;                                          //this allows writing all function for 2 DVD case
+        a_t_1 = discount(t);
+        a_t_2 = 1;                                              //arbitrary placeholder value
+        g_update(p_t_1, p_t_2, a_t_1, a_t_2, t);        //update g and store the revenue for the week
+    }
+    for (; t < BUNDLING_WEEK; t++) {                            //new DVD released. NO BUNDLING YET
+        p_t_1 = prices[t];
+        //p_t_2 = INFTY;
+        p_t_2 = prices[t - NEW_RELEASE_WEEK];
+        a_t_1 = discount(t);
+        a_t_2 = discount(t - NEW_RELEASE_WEEK);
+        g_update(p_t_1, p_t_2, a_t_1, a_t_2, t);
+    }
+    for (; t < time; t++){                                      //Bundle offered. Continue until end of tracking period
+        g->bundle = true;          //bundle se calculation will now be activated
+        p_t_1 = prices[t];
+        //p_t_2 = INFTY; 
+        p_t_2 = prices[t - NEW_RELEASE_WEEK];
+        a_t_1 = discount(t);
+        a_t_2 = discount(t - NEW_RELEASE_WEEK);
+        g_update(p_t_1, p_t_2, a_t_1, a_t_2, t);
+    }
+    return ;
+}
+
+double inline Bundle_Simulation::compute_val_both(int i, int j){
+	return (a_t_1*((double)i + 0.5) + a_t_2 *((double) j + 0.5))* g->step_size;
+}
+double inline Bundle_Simulation::compute_val_1(int i, int j){
+	return a_t_1 *((double) i + 0.5) * g->step_size;
+}
+double inline Bundle_Simulation::compute_val_2(int i, int j){
+	return a_t_1 *((double) i + 0.5) * g->step_size;
+}
+bool inline Bundle_Simulation::check_for_bundle(double p_b, int i, int j) {
+	double val_both = compute_val_both(i, j);
+	//return less_equal<double>(p_b, val_both);						// TBD
+	return (p_b <= val_both ? 1 : 0); 
+}
+bool inline Bundle_Simulation::bundle_check_p_1(double p_t_1, int i, int j){
+	double val_1 = compute_val_1(i, j);
+	//return less(val_1, (p_t_1 - 5) );
+	return (val_1 <= (p_t_1 - 5) ? 1 : 0);
+}
+bool inline Bundle_Simulation::bundle_check_p_2(double p_t_2, int i, int j){
+	double val_2 = compute_val_2(i, j);
+	//return less(val_2, (p_t_2 - 5) );
+	return (val_2 <= (p_t_2 - 5) ? 1:0 );
+}
+bool inline Bundle_Simulation::check_1(double p_t_1, int i, int j){
+	double val_1 = compute_val_1(i, j);
+	//return less_equal (val_1, p_t_1);
+	return (val_1 <= p_t_1 ? 1:0);
+}	
+bool inline Bundle_Simulation::check_2(double p_t_2, int i, int j){
+	double val_2 = compute_val_2(i, j);
+	//return less_equal(val_2, p_t_2);
+	return (val_2 <=  p_t_2 ? 1:0);
+}
+
+void Bundle_Simulation::g_update(double p_t_1, double p_t_2, double  a_t_1, double a_t_2, int t){
+	
+	double temp = 0;
+	double rev =0;
+	double p_b = p_t_1 + p_t_1 - 5;
+	for (int i = 0; i < g->num_boxes; i++){
+		for (int j = 0; j < g->num_boxes; j++) {
+			if ( check_for_bundle(p_b, i, j) && ( g->bundle == true ) ) {
+				if ( bundle_check_p_1(p_t_1, i, j) ) {
+					temp = g->buy_2(p_t_2, i, j);
+					rev = temp;
+				}
+				else if ( bundle_check_p_2(p_t_1, i, j) ) {
+					temp = g->buy_1(p_t_1, i, j);
+					rev = temp;
+				}	
+				else {
+					temp = g->buy_bundle(p_b, i, j);
+				}
+			}
+			else if ( check_1(p_t_1, i, j) ) {
+				if ( check_2(p_t_2, i, j) ) {
+					temp = g->buy_both(p_t_1, p_t_2, i, j);
+					rev += temp;
+				}
+				else { 
+					temp = g->buy_1(p_t_1, i, j);
+					rev += temp;
+
+				}
+			}
+			else if ( check_2(p_t_2, i, j) ) {
+				temp = g->buy_2(p_t_2, i, j);
+				rev += temp;
+			}
+			else{ 
+				g->buy_none(i, j);
+			}
+		} //close j loop
+	} //close i loop
+	revenue[t] = rev;
+}
+	
+
+class Independent:public G_Array {
+	public:
+		Independent(double eta, double phi, double  nu, double psi, double tau, int num_boxes, int prices_size);
+	//	~Independent(){;};
+		void initialize_array();
+		double* get_revenue(){return revenue;};
+		
+		
+
+};
+
+Independent::Independent(double eta, double phi, double  nu, double psi, double tau, int num_boxes, int prices_size):
+	 G_Array(eta, phi, nu, psi, tau, num_boxes, prices_size){
+
+	f = (double)1/(psi*psi);
+	initialize_array();
+	
+}
+//Independent::~Independent():~G_Array(){;}
+void Independent::initialize_array(){  
+
+int i, j;
+for (i=0; i < num_boxes; i++) { 
+	for (j=0; j < num_boxes; j++) {
+                 if( (i == num_boxes -1) || (j == num_boxes -1) ) {
+                         g[GIDX(i,j)] = 0;
+                        }
+                 else{
+                         g[GIDX(i,j)] = eta * f * step_size*step_size;
+                        }                       // number of people in each box of the approxmation for the g(v)
+        }
+    }
+
+
+}
+
+	
+
+int main() {
+	Independent * test;
+	test = new Independent (2000, .03, .005, 50, 3, 1501, 260);
+	delete test;
+	return 0;
+}
+ 
+		
+		
+
+		
+		
+
+
+	
+			
+			
+	
